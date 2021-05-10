3ada1ab04006c20d1fc62f77277a55e62fdae3d0
namirinz <nathmcfc31115@gmail.com>
2021-05-10 16:02:32 +0700

    Update

--- nami/torch/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from nami.torch.tools.engine import *
-from nami.torch.tools.coco_eval import *
-from nami.torch.tools.coco_utils import *
-from nami.torch.tools.utils import *
-from nami.torch.tools.transforms import *
